82d10f97ab9bbbc9e92f1252e63780aa4c5752ff
yujin <uuununew@gmail.com>
2025-04-07 19:34:41 +0900

    콘서트 - 포인트 충전(한도 추가)

--- src/main/java/kr/hhplus/be/server/domain/cash/UserCash.java
+++ src/main/java/kr/hhplus/be/server/domain/cash/UserCash.java
@@ -9,6 +9,7 @@ public class UserCash {
 
     private final Long userId;
     private BigDecimal amount;
+    private static final BigDecimal MAX_CASH_LIMIT = new BigDecimal("1_000_000"); //100만원 한도
 
     public UserCash(Long userId, BigDecimal initialAmount){
         if(userId == null) throw new IllegalArgumentException("userID는 null일 수 없습니다.");
@@ -21,6 +22,10 @@ public class UserCash {
 
     public void charge(BigDecimal value){
         validate(value);
+
+        if (this.amount.add(value).compareTo(MAX_CASH_LIMIT) > 0) {
+            throw new IllegalArgumentException("최대 충전 가능 금액을 초과했습니다.");
+        }
         this.amount = this.amount.add(value);
     }
 
