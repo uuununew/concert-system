8ea5d5f682ee6812c8a7b37c348b047fda7c5c71
yujin <uuununew@gmail.com>
2025-04-10 21:58:47 +0900

    cash - 도메인 엔티티 및 타입 정의 : 예외처리 수정

--- src/main/java/kr/hhplus/be/server/domain/cash/UserCash.java
+++ src/main/java/kr/hhplus/be/server/domain/cash/UserCash.java
@@ -1,6 +1,8 @@
 package kr.hhplus.be.server.domain.cash;
 
 import jakarta.persistence.*;
+import kr.hhplus.be.server.support.exception.CustomException;
+import kr.hhplus.be.server.support.exception.ErrorCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -39,7 +41,7 @@ public class UserCash {
      */
     public void charge(BigDecimal amount) {
         if (this.amount.add(amount).compareTo(MAX_CASH_LIMIT) > 0) {
-            throw new IllegalArgumentException("최대 충전 가능 금액을 초과했습니다.");
+            throw new CustomException(ErrorCode.INVALID_REQUEST, "최대 충전 가능 금액을 초과했습니다.");
         }
         this.amount = this.amount.add(amount);
     }
@@ -50,10 +52,10 @@ public class UserCash {
      */
     public void use(BigDecimal amount) {
         if (amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("0원 이하 금액은 사용할 수 없습니다.");
+            throw new CustomException(ErrorCode.INVALID_REQUEST, "0원 이하 금액은 사용할 수 없습니다.");
         }
         if (this.amount.compareTo(amount) < 0) {
-            throw new IllegalArgumentException("잔액이 부족합니다.");
+            throw new CustomException(ErrorCode.INVALID_REQUEST, "잔액이 부족합니다.");
         }
         this.amount = this.amount.subtract(amount);
     }
