2d02f6a1720f3b32e3c6176277bad0581ee5745b
yujin <uuununew@gmail.com>
2025-04-10 21:59:23 +0900

    cash - command 객체 및 유효성 검증 추가 : 예외처리 수정

--- src/main/java/kr/hhplus/be/server/application/cash/ChargeCashCommand.java
+++ src/main/java/kr/hhplus/be/server/application/cash/ChargeCashCommand.java
@@ -2,6 +2,8 @@ package kr.hhplus.be.server.application.cash;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
+import kr.hhplus.be.server.support.exception.CustomException;
+import kr.hhplus.be.server.support.exception.ErrorCode;
 import lombok.Getter;
 
 import java.math.BigDecimal;
@@ -14,10 +16,10 @@ public class ChargeCashCommand{
 
     public ChargeCashCommand(Long userId, BigDecimal amount) {
         if (userId == null || userId <= 0) {
-            throw new IllegalArgumentException("userId는 0보다 커야합니다. 없습니다.");
+            throw new CustomException(ErrorCode.UNAUTHORIZED);
         }
         if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("충전 금액은 0보다 커야 합니다.");
+            throw new CustomException(ErrorCode.INVALID_REQUEST, "충전 금액은 0보다 커야 합니다");
         }
         this.userId = userId;
         this.amount = amount;
--- src/main/java/kr/hhplus/be/server/application/cash/UseCashCommand.java
+++ src/main/java/kr/hhplus/be/server/application/cash/UseCashCommand.java
@@ -2,6 +2,8 @@ package kr.hhplus.be.server.application.cash;
 
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
+import kr.hhplus.be.server.support.exception.CustomException;
+import kr.hhplus.be.server.support.exception.ErrorCode;
 import lombok.Getter;
 
 import java.math.BigDecimal;
@@ -13,10 +15,10 @@ public class UseCashCommand {
 
     public UseCashCommand(Long userId, BigDecimal amount) {
         if (userId == null || userId <= 0) {
-            throw new IllegalArgumentException("userId는 0보다 커야합니다.");
+            throw new CustomException(ErrorCode.UNAUTHORIZED, "userId는 0보다 커야합니다.");
         }
         if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new IllegalArgumentException("사용 금액은 0보다 커야 합니다.");
+            throw new CustomException(ErrorCode.INVALID_REQUEST, "사용 금액은 0보다 커야 합니다.");
         }
         this.userId = userId;
         this.amount = amount;
