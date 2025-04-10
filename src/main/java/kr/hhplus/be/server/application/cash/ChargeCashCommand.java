33fe9feb1dc4887ce451ad9176da40edcddf44df
yujin <uuununew@gmail.com>
2025-04-10 18:43:43 +0900

    cash : command 객체 및 유효성 검증 추가

---
+++ src/main/java/kr/hhplus/be/server/application/cash/ChargeCashCommand.java
@@ -0,0 +1,27 @@
+package kr.hhplus.be.server.application.cash;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+public class ChargeCashCommand{
+
+    private final Long userId;
+    private final BigDecimal amount;
+
+    public ChargeCashCommand(Long userId, BigDecimal amount) {
+        if (userId == null || userId <= 0) {
+            throw new IllegalArgumentException("userId는 0보다 커야합니다. 없습니다.");
+        }
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("충전 금액은 0보다 커야 합니다.");
+        }
+        this.userId = userId;
+        this.amount = amount;
+    }
+
+}
+
