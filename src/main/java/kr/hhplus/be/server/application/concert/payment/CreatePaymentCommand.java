a0cb299156875849e33dbd8c9df967f7ea5e1f17
yujin <uuununew@gmail.com>
2025-04-11 00:36:41 +0900

    concert_payment : 결제 커맨드 정의

---
+++ src/main/java/kr/hhplus/be/server/application/concert/payment/CreatePaymentCommand.java
@@ -0,0 +1,24 @@
+package kr.hhplus.be.server.application.concert.payment;
+
+import kr.hhplus.be.server.support.exception.CustomException;
+import kr.hhplus.be.server.support.exception.ErrorCode;
+
+import java.math.BigDecimal;
+
+public record CreatePaymentCommand(
+        Long userId,
+        Long reservationId,
+        BigDecimal amount
+) {
+    public CreatePaymentCommand {
+        if (userId == null || userId <= 0) {
+            throw new CustomException(ErrorCode.UNAUTHORIZED, "userId는 필수이며 0보다 커야 합니다.");
+        }
+        if (reservationId == null || reservationId <= 0) {
+            throw new CustomException(ErrorCode.INVALID_REQUEST, "concertSeatId는 필수이며 0보다 커야 합니다.");
+        }
+        if (amount == null || amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new CustomException(ErrorCode.INVALID_REQUEST, "price는 필수이며 0보다 커야 합니다.");
+        }
+    }
+}
