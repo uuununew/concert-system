0e68aa89c828256adaded8289b08f3e26f9f3635
yujin <uuununew@gmail.com>
2025-04-13 23:29:53 +0900

    리팩토링 : enum에 description 필드 추가 및 의미 명확화

--- src/main/java/kr/hhplus/be/server/domain/concert/payment/PaymentStatus.java
+++ src/main/java/kr/hhplus/be/server/domain/concert/payment/PaymentStatus.java
@@ -1,8 +1,17 @@
 package kr.hhplus.be.server.domain.concert.payment;
 
+import lombok.Getter;
+
+@Getter
 public enum PaymentStatus {
-    READY,      // 결제 준비 상태
-    PAID,       // 결제 완료
-    CANCELED,   // 결제 취소
-    FAILED      // 결제 실패
+    READY("결제 준비 상태"),
+    PAID("결제 완료"),
+    CANCELED("결제 취소"),
+    FAILED("결제 실패");
+
+    private final String description;
+
+    PaymentStatus(String description) {
+        this.description = description;
+    }
 }
