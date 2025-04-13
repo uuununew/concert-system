9c68e49fbce8da92af6eca8733a6faee7c9ba36a
yujin <uuununew@gmail.com>
2025-04-13 22:58:56 +0900

    리팩토링 : Cash 도메인 Repository -> JPA 형태로 변경

--- src/main/java/kr/hhplus/be/server/domain/cash/CashHistoryRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/cash/CashHistoryRepository.java
@@ -1,11 +1,10 @@
 package kr.hhplus.be.server.domain.cash;
 
+import kr.hhplus.be.server.domain.concert.reservation.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CashHistoryRepository {
-    CashHistory save(CashHistory cashHistory);
-
+public interface CashHistoryRepository extends JpaRepository<CashHistory, Long> {
     List<CashHistory> findByUserId(Long userId);
 }
--- src/main/java/kr/hhplus/be/server/domain/cash/UserCashRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/cash/UserCashRepository.java
@@ -1,9 +1,10 @@
 package kr.hhplus.be.server.domain.cash;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
-public interface UserCashRepository {
+public interface UserCashRepository extends JpaRepository<UserCash, Long> {
     Optional<UserCash> findByUserId(Long userId);
-
-    UserCash save(UserCash userCash);
 }
+
