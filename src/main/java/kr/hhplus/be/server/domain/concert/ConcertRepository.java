59dba22496050a9392c796446396ef466178bb3d
yujin <uuununew@gmail.com>
2025-04-15 14:24:45 +0900

    concert 도메인 repository 구현체 및 서비스 의존성 적용

--- src/main/java/kr/hhplus/be/server/domain/concert/ConcertRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/concert/ConcertRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface ConcertRepository extends JpaRepository<Concert, Long> {
+public interface ConcertRepository{
     Optional<Concert> findById(Long id);
+    Concert save(Concert concert);
+    List<Concert> findAll();
+    boolean existsById(Long id);
 }
