7117485dd3ff22df81ef8b425c786359dc08bbf3
yujin <uuununew@gmail.com>
2025-04-15 12:57:36 +0900

    연관관계 : service - 연관관계 변경에 맞게 로직 수정 추가

--- src/main/java/kr/hhplus/be/server/domain/concert/ConcertSeatRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/concert/ConcertSeatRepository.java
@@ -7,7 +7,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ConcertSeatRepository extends JpaRepository<ConcertSeat, Long> {
-    List<ConcertSeat> findAllByConcertId(Long concertId);
 
-    List<ConcertSeat> findAllByConcertIdAndStatus(Long concertId, SeatStatus status);
+    List<ConcertSeat> findAllByConcert_Id(Long concertId);
+
+    List<ConcertSeat> findAllByConcert_IdAndStatus(Long concertId, SeatStatus status);
 }
