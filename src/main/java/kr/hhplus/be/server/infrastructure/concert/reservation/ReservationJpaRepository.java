4c7cf1b9d2bc9d7d57345c123132000aa82a7c15
yujin <uuununew@gmail.com>
2025-04-15 17:03:58 +0900

    reservation repository 구현 및 서비스 의존성 적용

--- src/main/java/kr/hhplus/be/server/infrastructure/concert/reservation/ReservationJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/concert/reservation/ReservationJpaRepository.java
@@ -1,15 +1,17 @@
-package kr.hhplus.be.server.domain.concert.reservation;
+package kr.hhplus.be.server.infrastructure.concert.reservation;
 
 import kr.hhplus.be.server.domain.concert.ConcertSeat;
+import kr.hhplus.be.server.domain.concert.reservation.Reservation;
+import kr.hhplus.be.server.domain.concert.reservation.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-public interface ReservationRepository{
-    Optional<Reservation> findById(Long id);
-    Reservation save(Reservation reservation);
+public interface ReservationJpaRepository extends JpaRepository<Reservation, Long> {
+
     List<Reservation> findAllByStatusAndCreatedAtBefore(ReservationStatus status, LocalDateTime cutoffTime);
     Optional<Reservation> findByConcertSeatAndStatus(ConcertSeat seat, ReservationStatus status);
 }
+
