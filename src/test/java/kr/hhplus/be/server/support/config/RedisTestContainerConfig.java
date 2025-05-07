22e0a8c9bd99ca3ec079880867d217c5b827e898
yujin <uuununew@gmail.com>
2025-05-07 13:53:54 +0900

    test: Redis 및 테스트컨테이너 설정 추가

---
+++ src/test/java/kr/hhplus/be/server/support/config/RedisTestContainerConfig.java
@@ -0,0 +1,44 @@
+package kr.hhplus.be.server.support.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@TestConfiguration
+public class RedisTestContainerConfig {
+
+    static final GenericContainer<?> REDIS_CONTAINER =
+            new GenericContainer<>("redis:7.0.5").withExposedPorts(6379);
+
+    static {
+        REDIS_CONTAINER.start();
+        System.out.println("🔧 Redis Container started at: " +
+                REDIS_CONTAINER.getHost() + ":" + REDIS_CONTAINER.getMappedPort(6379));
+    }
+
+    @DynamicPropertySource
+    static void overrideRedisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+        registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379));
+    }
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(
+                REDIS_CONTAINER.getHost(), REDIS_CONTAINER.getMappedPort(6379)
+        );
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        return new StringRedisTemplate(lettuceConnectionFactory);
+    }
+}
